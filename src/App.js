811107e00caf9c4b65a6e6d6fa0cba69133c9557
khloptsev <p.khloptsev@gmail.com>
2022-11-29 17:03:22 +1000

    добавлены новые компоненты

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+
 import Header from './components/header';
+import Title from './components/title/Title';
+import Sort from './components/sort/Sort';
+import Filter from './components/filter/Filter';
 
 const App = () => (
   <div className="wrapper">
     <Header />
+    <div className="content">
+      <div className="container">
+        <Title />
+        <Sort />
+        <Filter />
+      </div>
+    </div>
   </div>
 );
 
