05eff7f6c9c075cf3cf5a81aaad9331c1c5cdecd
khloptsev <p.khloptsev@gmail.com>
2022-11-29 17:01:56 +1000

    добавлен компонент Titile

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Title.module.scss';
+
+const Title = () => (
+  <div className={styles.root}>
+    <h1 className={styles.text}>Весь ассортимент</h1>
+  </div>
+);
+
+export default Title;
