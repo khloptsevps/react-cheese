370c5700d7f664c3980564b816b15e739d2a484b
khloptsev <p.khloptsev@gmail.com>
2022-11-29 17:03:09 +1000

    добавлен компонент сортировки

---
+++ src/components/sort/Sort.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import styles from './Sort.module.scss';
+
+const Sort = () => {
+  const sortMethod = [
+    { id: 1, name: 'Цене' },
+    { id: 2, name: 'Алфавиту' },
+  ];
+  const [activeSort, setActiveSort] = useState(1);
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>Сортировать по:</div>
+      <ul className={styles.list}>
+        {sortMethod.map(({ id, name }) => {
+          const active = id === activeSort ? styles.active : null;
+          return (
+            <li
+              className={`${styles.item} ${active}`}
+              key={id}
+              aria-hidden="true"
+              onClick={() => setActiveSort(id)}
+            >
+              <span>{name}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Sort;
