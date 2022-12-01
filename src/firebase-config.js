8f65c42afe7492029700bc43a3a8cce01a769eb0
khloptsev <p.khloptsev@gmail.com>
2022-12-01 17:52:22 +1000

    поправлен конфиг

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,7 +7,7 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBO-siB49NDdMT7M9v7m0sa9p-79PtvMLg',
+  apiKey: '',
   authDomain: 'react-cheese.firebaseapp.com',
   projectId: 'react-cheese',
   storageBucket: 'react-cheese.appspot.com',
