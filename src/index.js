78e34f71e106bd52edc3e043e2f0a97300d88ac5
khloptsev <p.khloptsev@gmail.com>
2022-12-07 11:09:58 +1000

    кастомизировал тему, добавил провайдер

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { createTheme, ThemeProvider } from '@mui/material';
+
 import App from './App.js';
 
 import './sass/main.scss';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+    htmlFontSize: 10,
+    fontSize: 16,
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+);
