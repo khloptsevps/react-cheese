34ee465bbdd30591842029f3c5ee13a89651da04
khloptsev <p.khloptsev@gmail.com>
2022-12-09 21:07:38 +1000

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { createTheme, ThemeProvider } from '@mui/material';
-
-import App from './App.js';
-
 import './sass/main.scss';
+import initApp from './initApp.js';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-    ].join(','),
-    htmlFontSize: 10,
-    fontSize: 16,
-  },
-  palette: {
-    primary: {
-      main: 'rgba(244, 178, 4)',
-    },
-    yellow: {
-      main: '#f4b204',
-    },
-  },
-  components: {
-    MuiChip: {
-      styleOverrides: {
-        root: {
-          color: '#000',
-          '&:hover': {
-            backgroundColor: 'rgba(244, 178, 4, 0.7)',
-          },
-        },
-      },
-    },
-    MuiCardMedia: {
-      styleOverrides: {
-        root: {
-          height: 250,
-          objectFit: 'cover',
-        },
-      },
-    },
-  },
-});
+const app = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const myApp = initApp();
+  root.render(myApp);
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-);
+app();
