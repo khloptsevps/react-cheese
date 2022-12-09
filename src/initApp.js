ff6d98e955a6fcd4d99f0256dde9f9832002ea1d
khloptsev <p.khloptsev@gmail.com>
2022-12-09 21:07:25 +1000

    added initApp func

---
+++ src/initApp.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material';
+import App from './App';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+    htmlFontSize: 10,
+    fontSize: 16,
+  },
+  palette: {
+    primary: {
+      main: 'rgba(244, 178, 4)',
+    },
+    yellow: {
+      main: '#f4b204',
+    },
+  },
+  components: {
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          color: '#000',
+          '&:hover': {
+            backgroundColor: 'rgba(244, 178, 4, 0.7)',
+          },
+        },
+      },
+    },
+    MuiCardMedia: {
+      styleOverrides: {
+        root: {
+          height: 250,
+          objectFit: 'cover',
+        },
+      },
+    },
+  },
+});
+
+const initApp = () => {
+  const app = (
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  );
+  return app;
+};
+
+export default initApp;
