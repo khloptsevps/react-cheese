b5555828cd2e29a543019ddb8bc376cd1e66bea4
khloptsev <p.khloptsev@gmail.com>
2022-12-09 20:45:42 +1000

    обновлен компонент  sort

--- src/components/sort/Sort.jsx
+++ src/components/sort/Sort.jsx
@@ -5,27 +5,67 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import styles from './Sort.module.scss';
 
 const Sort = () => {
-  const sortMethod = [
-    { id: 1, name: 'Цене' },
-    { id: 2, name: 'Алфавиту' },
+  const sortMethods = [
+    { id: 1, name: 'цене' },
+    { id: 2, name: 'алфавиту' },
   ];
   const [activeSort, setActiveSort] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const sortMethod = sortMethods.find(({ id }) => id === activeSort);
+
+  const sortRef = React.useRef(null);
+
+  const handleClickOutside = (e) => {
+    if (!e.path.includes(sortRef.current)) {
+      setIsOpen(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  });
 
   return (
-    <div className={styles.root}>
+    <div className={styles.root} ref={sortRef}>
       <div className={styles.label}>
         <ArrowDropDownIcon />
         <span className={styles.title}>Сортировка по:</span>
         <span
           className={styles.condition}
-          onClick={() => {}}
-          role="button"
-          tabIndex={0}
+          onClick={() => setIsOpen(!isOpen)}
           aria-hidden="true"
         >
-          цене
+          {sortMethod.name}
         </span>
       </div>
+      {isOpen && (
+        <div className={styles.popup}>
+          <ul>
+            {sortMethods.map(({ id, name }) => {
+              const isActive = id === activeSort ? styles.active : '';
+              return (
+                <li
+                  className={isActive}
+                  key={id}
+                  onClick={() => {
+                    setActiveSort(id);
+                    setIsOpen(!isOpen);
+                  }}
+                  aria-hidden="true"
+                >
+                  {name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
