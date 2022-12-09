731e826f9717faa52b3003bce6567999517e29a0
khloptsev <p.khloptsev@gmail.com>
2022-12-09 14:05:10 +1000

    обновлен theme

--- src/index.js
+++ src/index.js
@@ -20,11 +20,24 @@ const theme = createTheme({
     fontSize: 16,
   },
   palette: {
+    primary: {
+      main: 'rgba(244, 178, 4)',
+    },
     yellow: {
       main: '#f4b204',
     },
   },
   components: {
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          color: '#000',
+          '&:hover': {
+            backgroundColor: 'rgba(244, 178, 4, 0.7)',
+          },
+        },
+      },
+    },
     MuiCardMedia: {
       styleOverrides: {
         root: {
