35913e595bc526e74971441db7e296fe8637a8a1
khloptsev <p.khloptsev@gmail.com>
2022-12-12 22:48:26 +1000

    функция нормализации данных с firebase и сорт.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,17 @@
-const firebaseDocsToArray = (firebaseSnapshot) => {
+const firebaseDocsToObjects = (firebaseSnapshot, sortBy, order) => {
   const products = firebaseSnapshot.docs.map((document) => {
     const item = { id: document.id, ...document.data() };
     return item;
   });
+  if (sortBy === 'name') {
+    const productsSortedByName = products.sort((a, b) =>
+      a.name.localeCompare(b.name),
+    );
+    return order === 'asc'
+      ? productsSortedByName
+      : productsSortedByName.reverse();
+  }
   return products;
 };
 
-export default firebaseDocsToArray;
+export default firebaseDocsToObjects;
