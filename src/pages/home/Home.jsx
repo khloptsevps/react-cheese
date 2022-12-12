221ead6311ec3ae2519bf9bafaad0522edb39359
khloptsev <p.khloptsev@gmail.com>
2022-12-12 14:11:34 +1000

    реализован рендер всех продуктов

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectProducts } from '../../redux/slices/productSlice';
 
 import Title from '../../components/title/Title';
 import Sort from '../../components/sort/Sort';
-import Filter from '../../components/filter/Filter';
+// import Filter from '../../components/filter/Filter';
+import ProductCard from '../../components/card/ProductCard';
+import Skeleton from '../../components/skeletons/Skeleton';
 
 import styles from './Home.module.scss';
 
-const HomePage = () => (
-  <div className={styles.content}>
-    <div className={styles.container}>
-      <div className={styles.top}>
-        <Filter />
-        <Sort />
-      </div>
-      <div className={styles.title}>
-        <Title />
-        <div className={styles.cards}></div>
+const HomePage = () => {
+  const { items, process } = useSelector(selectProducts);
+  return (
+    <div className={styles.content}>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          {/* <Filter /> */}
+          <Sort />
+        </div>
+        <div className={styles.title}>
+          <Title />
+          <div className={styles.cards}>
+            {process === 'success'
+              ? items.map((item) => <ProductCard key={item.id} {...item} />)
+              : Array(6)
+                  .fill()
+                  .map((_item, i) => <Skeleton key={i} />)}
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+// TODO временно отключен фильтр
 
 export default HomePage;
