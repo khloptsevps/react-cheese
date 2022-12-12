4816d01efe357bcbdab252943a3f949a3e3c957c
khloptsev <p.khloptsev@gmail.com>
2022-12-12 22:45:26 +1000

    добавлен filterSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import productSlice from './slices/productSlice';
+import filterSlice from './slices/filterSlice';
 
 const store = configureStore({
   reducer: {
     products: productSlice,
+    filter: filterSlice,
   },
 });
 
