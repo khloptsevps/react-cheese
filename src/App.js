5c28b150b30ad7dd904aba12c4b6f1e3a037a0bd
khloptsev <p.khloptsev@gmail.com>
2022-12-15 22:26:22 +1000

    добавил модальное окно

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 
 import Header from './components/header';
+import Modal from './components/modal/Modal';
 import Home from './pages/home/Home';
 import NotFound from './pages/notFound/NotFound';
 
@@ -29,7 +30,10 @@ const router = createBrowserRouter([
 const App = () => (
   <div className="wrapper">
     <RouterProvider router={router} />
+    <Modal />
   </div>
 );
 
 export default App;
+
+// TODO: подумать куда поместить Modal;
