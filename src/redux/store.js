97c8ce2d76db6b060474c91fc0bd154ea7c8eb91
khloptsev <p.khloptsev@gmail.com>
2022-12-15 22:28:57 +1000

    добавлен редьюсер модального окна

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import productSlice from './slices/productSlice';
 import filterSlice from './slices/filterSlice';
+import modalSlice from './slices/modalSlice';
 
 const store = configureStore({
   reducer: {
     products: productSlice,
     filter: filterSlice,
+    modal: modalSlice,
   },
 });
 
