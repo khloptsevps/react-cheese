af24217747500920827348488253a7e4336361f2
khloptsev <p.khloptsev@gmail.com>
2023-01-18 11:43:59 +1000

    update cart link and added styles

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-
-import cartIcon from '../../assets/svg/cart-icon.svg';
+import { Link } from 'react-router-dom';
+import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 
 import styles from './Cart.module.scss';
 
 const Cart = () => (
-  <div className={styles.root}>
-    <div className={styles.items}>
-      <div className={styles.box}>
-        <a href="/" className={styles.link}>
-          <div className={styles.iconBox}>
-            <img src={cartIcon} alt="icon_cart" className={styles.icon} />
-            <span className={styles.count}>5</span>
-          </div>
-          <span className={styles.price}>1 500 ₽</span>
-        </a>
-      </div>
-    </div>
+  <div>
+    <Link to="/cart" className={styles.link}>
+      <span>1 500 ₽</span>
+      <div className={styles.delimiter}></div>
+      <ShoppingBasketOutlinedIcon fontSize="small" className="mr-10" />
+      <span>3</span>
+    </Link>
   </div>
 );
 
