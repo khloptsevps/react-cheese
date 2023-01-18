8163d5dd05582d429adcf7bdba0553028351170e
khloptsev <p.khloptsev@gmail.com>
2023-01-18 17:56:26 +1000

    add cartSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import productSlice from './slices/productSlice';
 import filterSlice from './slices/filterSlice';
 import modalSlice from './slices/modalSlice';
+import cartSlice from './slices/cartSlice';
 
 const store = configureStore({
   reducer: {
     products: productSlice,
     filter: filterSlice,
     modal: modalSlice,
+    cart: cartSlice,
   },
 });
 
