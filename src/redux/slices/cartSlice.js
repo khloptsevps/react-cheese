a4e36d5f5fffa4566634695bf16b2c9ca31f8421
khloptsev <p.khloptsev@gmail.com>
2023-01-18 17:55:40 +1000

    added addItem reducer

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
@@ -8,9 +9,25 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    addItem: (state, { payload }) => {
+      const { items } = state;
+      const findItem = items.find((item) => item.id === payload.id);
+      if (!findItem) {
+        items.push({ ...payload, count: 1 });
+      } else {
+        findItem.count += 1;
+      }
+      state.totalPrice = items.reduce(
+        (acc, item) => acc + item.count * item.price,
+        0,
+      );
+    },
+  },
 });
 
-// export const {} = cartSlice.actions
+export const { addItem } = cartSlice.actions;
+
+export const selectCart = (state) => state.cart;
 
 export default cartSlice.reducer;
