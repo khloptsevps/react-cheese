ae10dc89a78f74a2f9830ef2d3471c64d7a80558
khloptsev <p.khloptsev@gmail.com>
2023-02-02 12:10:40 +1000

    remove temp eslint rule

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
