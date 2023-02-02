9313b7520b82eed884d1b7f657aadc04d61d50f9
khloptsev <p.khloptsev@gmail.com>
2023-02-02 14:49:28 +1000

    add totalItemsCount

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -10,13 +10,14 @@ import { selectCart } from '../../redux/slices/cartSlice.js';
 
 const Cart = () => {
   const { totalPrice, items } = useSelector(selectCart);
+  const totalItems = items.reduce((total, item) => total + item.count, 0);
   return (
     <div>
       <Link to="/cart" className={styles.link}>
         <span>{totalPrice.toLocaleString('ru')} ₽</span>
         <div className={styles.delimiter}></div>
         <ShoppingBasketOutlinedIcon fontSize="small" className="mr-10" />
-        <span>{items.length}</span>
+        <span>{totalItems}</span>
       </Link>
     </div>
   );
